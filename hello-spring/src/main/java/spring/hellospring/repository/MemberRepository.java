753330c563aadf8702567386eaad9e83bb6b43d5
noteasymin <noteasymin@gmail.com>
2023-01-03 23:51:06 +0900

    practice

---
+++ hello-spring/src/main/java/spring/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package spring.hellospring.repository;
+
+import org.springframework.stereotype.Repository;
+import spring.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
